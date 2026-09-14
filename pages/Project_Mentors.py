10e02553fd1fcd83d54586d99319b3677efd67de
Prastutee <prastuteeborah986@gmail.com>
2026-09-14 21:05:35 +0530

    Update page configuration for Project Mentors

--- pages/Project_Mentors.py
+++ pages/Project_Mentors.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-from navigation import render_top_navbar
+
 
 st.set_page_config(
     page_title="Architecture & Mentors | LoRA NMT",
